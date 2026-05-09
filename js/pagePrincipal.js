c41c406da1172b335d4f014b069cd52c6595c3c6
beatrizfernandesdev <emailmpfvs@gmail.com>
2026-05-09 14:45:52 -0300

    Finnal Commit

--- js/pagePrincipal.js
+++ js/pagePrincipal.js
@@ -7,3 +7,27 @@ const btnVoltar = document.getElementById('backToIndexPage');
 btnVoltar.addEventListener("click", () => {
      redirectTo("../index.html");
 });
+
+const audio = document.getElementById("bg-music");
+
+audio.volume = 0.3;
+
+// remove mute ao clicar na tela
+document.addEventListener("click", () => {
+
+     audio.muted = false;
+     audio.play();
+
+}, { once: true });
+
+// repetir após 3 segundos
+audio.addEventListener("ended", () => {
+
+     setTimeout(() => {
+
+          audio.currentTime = 0;
+          audio.play();
+
+     }, 10000);
+
+});
